26bb63d0f8093d255edb6398955a0aab6cf11e41
Kalash Jain <kalashjain446@gmail.com>
2023-10-03 13:18:49 +0530

    Commented some api

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-require("dotenv").config();
 const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const cookieParser = require("cookie-parser")
-const multer = require("multer")
 const path = require("path")
-const UserModel = require('./models/UserModel');
-const PostModel = require('./models/PostModel')
-const { time, timeStamp, log } = require("console")
-const fs = require('fs');
-const passport=require("passport");
-const cookieSession=require("cookie-session");
-const passportSetup=require("./Passport");
-const authRoute=require('./Routes/auth');
-const defaultPath = 'public/Images/';
 const PORT = process.env.PORT ||3001||8080
 const app = express();
-const userRoute=require('./Routes/userRoute');
-const postRoute=require('./Routes/postRoute');
-app.use(express.json());
-app.use(cookieParser());
+// require("dotenv").config();
+// const mongoose = require("mongoose")
+// const cors = require("cors")
+// const bcrypt = require("bcrypt")
+// const jwt = require("jsonwebtoken")
+// const cookieParser = require("cookie-parser")
+// const multer = require("multer")
+// const UserModel = require('./models/UserModel');
+// const PostModel = require('./models/PostModel')
+// const { time, timeStamp, log } = require("console")
+// const fs = require('fs');
+// const passport=require("passport");
+// const cookieSession=require("cookie-session");
+// const passportSetup=require("./Passport");
+// const authRoute=require('./Routes/auth');
+// const defaultPath = 'public/Images/';
+// const userRoute=require('./Routes/userRoute');
+// const postRoute=require('./Routes/postRoute');
+// app.use(express.json());
+// app.use(cookieParser());
 
-app.use(cookieSession({
-  name:"session",
-  keys:["kalash"],
-  maxAge:24*60*69*100,
-}))
+// app.use(cookieSession({
+//   name:"session",
+//   keys:["kalash"],
+//   maxAge:24*60*69*100,
+// }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use(cors({
-  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-  method: ["GET", "POST", "DELETE", "PUT"],
-  credentials: true
-}))
+// app.use(cors({
+//   origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+//   method: ["GET", "POST", "DELETE", "PUT"],
+//   credentials: true
+// }))
+
+// app.use('/auth',authRoute);
+// app.use('/user',userRoute);
+// app.use('/post',postRoute);
+// mongoose.connect(process.env.MONGO_URL)
+// mongoose.connect("mongodb://127.0.0.1:27017/Blog");
+// app.use(express.static('public')); // It is used to grant access to the static public folder to access the images .
 
 app.get("/test",(req,res)=>{
   res.send("This is testing route");
 })
-app.use('/auth',authRoute);
-app.use('/user',userRoute);
-app.use('/post',postRoute);
-mongoose.connect(process.env.MONGO_URL)
-// mongoose.connect("mongodb://127.0.0.1:27017/Blog");
-// app.use(express.static('public')); // It is used to grant access to the static public folder to access the images .
+
 app.listen(PORT, () => {
   console.log(`Connected to PORT ${PORT}`);
 })
