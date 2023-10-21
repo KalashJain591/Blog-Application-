5b3950ae995d5ec782b81e3c0ccd284a8a66f6d6
Kalash Jain <kalashjain446@gmail.com>
2023-10-22 00:01:21 +0530

    solved password , firebase errors .

---
+++ client/src/LoadingSpinner.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+ import { usePromiseTracker } from "react-promise-tracker";
+
+export const LoadingSpiner = (props) => {
+ const { promiseInProgress } = usePromiseTracker({delay:5000});
+
+  return (
+    <div>
+    {
+    (promiseInProgress === true) ?
+        <h3>Hey I'm a spinner loader wannabe !!!</h3>
+      :
+        null  
+    }
+  </div>
+  )
+};
+
+
